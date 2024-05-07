b594c540655fe68827f0a81d3ce281ffc8e0aea5
namtndus <rnrmf0721@gmail.com>
2024-05-07 14:20:53 +0900

    [feat]: ALS-48 회원가입 구현

--- src/main/java/com/als/webIde/controller/MemberController.java
+++ src/main/java/com/als/webIde/controller/MemberController.java
@@ -1,11 +1,13 @@
 package com.als.webIde.controller;
 
 import com.als.webIde.DTO.request.UserId;
+import com.als.webIde.DTO.request.UserInfo;
 import com.als.webIde.DTO.request.UserNickName;
 import com.als.webIde.domain.entity.Member;
 import com.als.webIde.domain.entity.MemberSetting;
 import com.als.webIde.domain.repository.MemberRepositpory;
 import com.als.webIde.domain.repository.MemberSettingRepository;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -38,6 +40,24 @@ public class MemberController {
         }
 
         return ResponseEntity.status(400).body("사용할 수 없는 닉네임입니다.");
+    }
+
+    @Transactional
+    @GetMapping("/signup")
+    public ResponseEntity<String> singUp(@RequestBody UserInfo userInfo){
+        if(!userInfo.getPassword().equals(userInfo.getPasswordConfirm())){
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다");
+        }
+        Member saveUser = Member.builder()
+                .userId(userInfo.getUserId())
+                .password(userInfo.getPassword()).build();
+        Member member = memberRepositpory.save(saveUser);
+        MemberSetting saveUserSetting = MemberSetting.builder()
+                .member(member)
+                .nickname(userInfo.getNickname()).build();
+
+        memberSettingRepository.save(saveUserSetting);
+        return ResponseEntity.ok("ok");
     }
 
 }
