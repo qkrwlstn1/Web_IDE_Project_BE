695d3847124eec18d8c9d1d3e03c05bd604da17f
namtndus <rnrmf0721@gmail.com>
2024-05-07 14:14:02 +0900

    [feat]: ALS-13 아이디 중복 검사 구현

--- src/main/java/com/als/webIde/controller/MemberController.java
+++ src/main/java/com/als/webIde/controller/MemberController.java
@@ -1,4 +1,29 @@
 package com.als.webIde.controller;
 
+import com.als.webIde.DTO.request.UserId;
+import com.als.webIde.domain.entity.Member;
+import com.als.webIde.domain.repository.MemberRepositpory;
+import com.als.webIde.domain.repository.MemberSettingRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/user")
 public class MemberController {
+
+    private final MemberRepositpory memberRepositpory;
+
+    @GetMapping("/idcheck")
+    public ResponseEntity<String> checkedUserId(@RequestBody UserId userId){
+        List<Member> memberByUserId = memberRepositpory.findMemberByUserId(userId.getUserId());
+        if (memberByUserId.isEmpty()){
+            return ResponseEntity.ok("사용할 수 있는 아이디입니다.");
+        }
+        return ResponseEntity.status(400).body("사용할 수 없는 아이디입니다.");
+    }
+
 }
