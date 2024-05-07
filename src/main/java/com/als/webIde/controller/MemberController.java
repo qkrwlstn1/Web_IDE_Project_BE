222fc3c9d77abe4af52dcbe2468a4f1a071d5ce5
namtndus <rnrmf0721@gmail.com>
2024-05-07 14:17:34 +0900

    [feat]: ALS-14 비밀번호 중복 검사 구현

--- src/main/java/com/als/webIde/controller/MemberController.java
+++ src/main/java/com/als/webIde/controller/MemberController.java
@@ -1,7 +1,9 @@
 package com.als.webIde.controller;
 
 import com.als.webIde.DTO.request.UserId;
+import com.als.webIde.DTO.request.UserNickName;
 import com.als.webIde.domain.entity.Member;
+import com.als.webIde.domain.entity.MemberSetting;
 import com.als.webIde.domain.repository.MemberRepositpory;
 import com.als.webIde.domain.repository.MemberSettingRepository;
 import lombok.AllArgsConstructor;
@@ -16,6 +18,7 @@ import java.util.List;
 public class MemberController {
 
     private final MemberRepositpory memberRepositpory;
+    private final MemberSettingRepository memberSettingRepository;
 
     @GetMapping("/idcheck")
     public ResponseEntity<String> checkedUserId(@RequestBody UserId userId){
@@ -24,6 +27,17 @@ public class MemberController {
             return ResponseEntity.ok("사용할 수 있는 아이디입니다.");
         }
         return ResponseEntity.status(400).body("사용할 수 없는 아이디입니다.");
+    }
+
+    @GetMapping("/nicknamecheck")
+    public ResponseEntity<String> checkedUserNickName(@RequestBody UserNickName nickName){
+        List<MemberSetting> findNickName = memberSettingRepository.findMemberSettingByNickname(nickName.getUserNickName());
+
+        if (findNickName.isEmpty()){
+            return ResponseEntity.ok("사용할 수 있는 닉네임입니다.");
+        }
+
+        return ResponseEntity.status(400).body("사용할 수 없는 닉네임입니다.");
     }
 
 }
--- src/main/java/com/als/webIde/domain/entity/MemberSetting.java
+++ src/main/java/com/als/webIde/domain/entity/MemberSetting.java
@@ -3,6 +3,9 @@ package com.als.webIde.domain.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 @Getter
 @Entity
 @Table(name = "member_setting")
@@ -10,15 +13,41 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MemberSetting {
-    @Id
+
+    @EmbeddedId
+    private MemberSettingId MemberId;
+
+    @MapsId("member")
     @OneToOne
-    @JoinColumn(name = "member_pk")
+    @JoinColumn(name = "user_pk")
     private Member member;
 
+    @Builder.Default
     @Column(name = "Thema", nullable = false)
-    private String thema;
+    private String thema="white";
 
     @Column(name = "nickname", nullable = false)
     private String nickname;
 
 }
+
+@NoArgsConstructor
+@Embeddable
+class MemberSettingId implements Serializable {
+
+    private Long member;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MemberSettingId memberId = (MemberSettingId) o;
+        return Objects.equals(member, memberId.member);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(member);
+    }
+}
+
