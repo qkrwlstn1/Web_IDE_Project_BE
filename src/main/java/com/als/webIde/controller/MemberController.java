cfc3d96fb7535b0dcf3be1e9e0991fbe09ed0e9b
namtndus <rnrmf0721@gmail.com>
2024-05-09 14:23:51 +0900

    [feat]: 로그인 구현

--- src/main/java/com/als/webIde/controller/MemberController.java
+++ src/main/java/com/als/webIde/controller/MemberController.java
@@ -1,28 +1,37 @@
 package com.als.webIde.controller;
 
+import com.als.webIde.DTO.etc.TokenDto;
 import com.als.webIde.DTO.request.UserId;
 import com.als.webIde.DTO.request.UserInfo;
+import com.als.webIde.DTO.request.UserLogin;
 import com.als.webIde.DTO.request.UserNickName;
 import com.als.webIde.domain.entity.Member;
 import com.als.webIde.domain.entity.MemberSetting;
 import com.als.webIde.domain.repository.MemberRepositpory;
 import com.als.webIde.domain.repository.MemberSettingRepository;
+import com.als.webIde.service.UserService;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@AllArgsConstructor
+@Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/user")
 public class MemberController {
 
     private final MemberRepositpory memberRepositpory;
     private final MemberSettingRepository memberSettingRepository;
+    private final UserService userService;
 
-    @GetMapping("/idcheck")
+    @PostMapping("/idcheck")
     public ResponseEntity<String> checkedUserId(@RequestBody UserId userId){
         List<Member> memberByUserId = memberRepositpory.findMemberByUserId(userId.getUserId());
         if (memberByUserId.isEmpty()){
@@ -31,7 +40,7 @@ public class MemberController {
         return ResponseEntity.status(400).body("사용할 수 없는 아이디입니다.");
     }
 
-    @GetMapping("/nicknamecheck")
+    @PostMapping("/nicknamecheck")
     public ResponseEntity<String> checkedUserNickName(@RequestBody UserNickName nickName){
         List<MemberSetting> findNickName = memberSettingRepository.findMemberSettingByNickname(nickName.getUserNickName());
 
@@ -43,21 +52,45 @@ public class MemberController {
     }
 
     @Transactional
-    @GetMapping("/signup")
-    public ResponseEntity<String> singUp(@RequestBody UserInfo userInfo){
+    @PostMapping("/signup")
+    public ResponseEntity<String> signUp(@RequestBody UserInfo userInfo){
         if(!userInfo.getPassword().equals(userInfo.getPasswordConfirm())){
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다");
         }
+        log.info("회원가입 시작!");
         Member saveUser = Member.builder()
                 .userId(userInfo.getUserId())
                 .password(userInfo.getPassword()).build();
         Member member = memberRepositpory.save(saveUser);
-        MemberSetting saveUserSetting = MemberSetting.builder()
+        log.info("유저은 : {}",saveUser.getUserId());
+        //MemberSettingId memberSettingId = new MemberSettingId(member.getUserPk());
+        MemberSetting saveUserSetting = new MemberSetting.builder()
                 .member(member)
                 .nickname(userInfo.getNickname()).build();
+        log.info("유저의 세팅은 : {}",saveUserSetting.getMemberId());
 
         memberSettingRepository.save(saveUserSetting);
         return ResponseEntity.ok("ok");
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody UserLogin userLogin) {
+        List<Member> memberByUserId = memberRepositpory.findMemberByUserId(userLogin.getUserId());
+        if(memberByUserId.isEmpty()){
+            // 에러 핸들러 설정하기
+            return ResponseEntity.ok("등록되지 않은 아이디입니다.");
+        }
+        Member member = memberByUserId.get(0);
+        if (!member.getPassword().equals(userLogin.getPassword())){
+            return ResponseEntity.ok("비밀번호가 틀렸습니다");
+        }
+
+        TokenDto token = userService.login(member.getUserPk());
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization",token.getAccessToken());
+        httpHeaders.add("X-Refrush-Token",token.getRefreshToken());
+
+        return ResponseEntity.ok().headers(httpHeaders).body("dfs");
+    }
 }
--- src/main/java/com/als/webIde/domain/entity/MemberSetting.java
+++ src/main/java/com/als/webIde/domain/entity/MemberSetting.java
@@ -9,7 +9,6 @@ import java.util.Objects;
 @Getter
 @Entity
 @Table(name = "member_setting")
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class MemberSetting {
@@ -18,36 +17,62 @@ public class MemberSetting {
     private MemberSettingId MemberId;
 
     @MapsId("member")
-    @OneToOne
+    @OneToOne(cascade = CascadeType.MERGE)
     @JoinColumn(name = "user_pk")
     private Member member;
 
-    @Builder.Default
     @Column(name = "Thema", nullable = false)
-    private String thema="white";
+    private String thema;
 
     @Column(name = "nickname", nullable = false)
     private String nickname;
 
+    public static class builder{
+        private Member member;
+        private String nickname;
+
+        public builder member(Member member){
+            this.member = member;
+            return this;
+        }
+
+        public builder nickname(String nickname){
+            this.nickname = nickname;
+            return this;
+        }
+
+        public MemberSetting build(){
+            return new MemberSetting(new MemberSettingId(member.getUserPk()),member,"white",nickname);
+        }
+    }
+
+
 }
 
-@NoArgsConstructor
+@Getter
+@Setter
 @Embeddable
+@NoArgsConstructor
 class MemberSettingId implements Serializable {
 
-    private Long member;
+    @Column(name = "user_pk")
+    private Long userPk;
+
+     MemberSettingId(Long userPk){
+        this.userPk=userPk;
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         MemberSettingId memberId = (MemberSettingId) o;
-        return Objects.equals(member, memberId.member);
+        return Objects.equals(userPk, memberId.userPk);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(member);
+        return Objects.hash(userPk);
     }
 }
 
